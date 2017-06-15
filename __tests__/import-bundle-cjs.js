eb0443b4ff11f6b331f95bf767b6941bad2df92e
Vlad Zhukov <work4vlad@gmail.com>
2017-06-15 12:24:00 +0300

    Add simple bundle tests

---
+++ __tests__/import-bundle-cjs.js
@@ -0,0 +1,17 @@
+const {
+    reduxStatus,
+    reducer,
+    selectors,
+    actions,
+    actionTypes
+} = require('../dist/redux-status.cjs');
+
+describe('import-bundle-cjs', () => {
+    it('should export properly', () => {
+        expect(typeof reduxStatus).toBe('function');
+        expect(typeof reducer).toBe('function');
+        expect(typeof selectors).toBe('object');
+        expect(typeof actions).toBe('object');
+        expect(typeof actionTypes).toBe('object');
+    });
+});
---
+++ __tests__/import-bundle-esm.js
@@ -0,0 +1,17 @@
+import {
+    reduxStatus,
+    reducer,
+    selectors,
+    actions,
+    actionTypes
+} from '../dist/redux-status.esm';
+
+describe('import-bundle-esm', () => {
+    it('should export properly', () => {
+        expect(typeof reduxStatus).toBe('function');
+        expect(typeof reducer).toBe('function');
+        expect(typeof selectors).toBe('object');
+        expect(typeof actions).toBe('object');
+        expect(typeof actionTypes).toBe('object');
+    });
+});
