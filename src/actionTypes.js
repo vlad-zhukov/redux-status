b912b70583085ff01536b910806630fc224db7b2
Vlad Zhukov <work4vlad@gmail.com>
2017-06-15 11:42:19 +0300

    Rename '_prefix' to 'prefix'

--- src/actionTypes.js
+++ src/actionTypes.js
@@ -1,5 +1,4 @@
-// eslint-disable-next-line no-underscore-dangle
-export const _prefix = '@@redux-status';
-export const INITIALIZE = `${_prefix}/INITIALIZE`;
-export const DESTROY = `${_prefix}/DESTROY`;
-export const UPDATE = `${_prefix}/UPDATE`;
+export const prefix = '@@redux-status/';
+export const INITIALIZE = `${prefix}INITIALIZE`;
+export const DESTROY = `${prefix}DESTROY`;
+export const UPDATE = `${prefix}UPDATE`;
