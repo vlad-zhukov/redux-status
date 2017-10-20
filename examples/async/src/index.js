0cbff733ea8729658861076b282a1c1eb0d502da
Vlad Zhukov <work4vlad@gmail.com>
2017-10-20 15:15:37 +0300

    Use Redux Devtools in an example

--- examples/async/src/index.js
+++ examples/async/src/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {combineReducers, createStore, applyMiddleware} from 'redux';
+import {combineReducers, createStore} from 'redux';
 import {Provider} from 'react-redux';
 import {reducer as statusReducer} from 'redux-status';
-import {createLogger} from 'redux-logger';
 import App from './containers/App';
 
 const reducer = combineReducers({
     status: statusReducer,
 });
 
-const middleware = [];
-if (process.env.NODE_ENV !== 'production') {
-    middleware.push(createLogger({collapsed: true, duration: true}));
+let enhancer;
+/* eslint-disable no-underscore-dangle */
+if (window.__REDUX_DEVTOOLS_EXTENSION__) {
+    enhancer = window.__REDUX_DEVTOOLS_EXTENSION__();
 }
+/* eslint-enable no-underscore-dangle */
 
-const store = createStore(reducer, applyMiddleware(...middleware));
+const store = createStore(reducer, enhancer);
 
 render(
     <Provider store={store}>
